d814e1b9a0a50fc1b07d3a0058447ecf4633b756
ungerecht <kevinungerecht@gmail.com>
2021-11-15 14:38:41 -0800

    Add watchlist actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,12 +1,12 @@
 import coingecko from "../apis/coingecko";
-import { SIGN_IN, SIGN_OUT, FETCH_COINS, SWITCH_THEME } from "./types";
-
-export const switchTheme = (theme) => {
-  return {
-    type: SWITCH_THEME,
-    payload: theme,
-  };
-};
+import {
+  SIGN_IN,
+  SIGN_OUT,
+  FETCH_COINS,
+  SWITCH_THEME,
+  ADD_COIN,
+  REMOVE_COIN,
+} from "./types";
 
 export const signIn = (userId) => {
   return {
@@ -22,7 +22,27 @@ export const signOut = () => {
 };
 
 export const fetchCoins = () => async (dispatch) => {
-  console.log("fetching coins");
   const response = await coingecko.get("/list");
   dispatch({ type: FETCH_COINS, payload: response.data });
+};
+
+export const switchTheme = (theme) => {
+  return {
+    type: SWITCH_THEME,
+    payload: theme,
+  };
+};
+
+export const addCoin = (id) => {
+  return {
+    type: ADD_COIN,
+    payload: id,
+  };
+};
+
+export const removeCoin = (id) => {
+  return {
+    type: REMOVE_COIN,
+    payload: id,
+  };
 };
--- src/actions/types.js
+++ src/actions/types.js
@@ -2,3 +2,5 @@ export const SIGN_IN = "SIGN_IN";
 export const SIGN_OUT = "SIGN_OUT";
 export const FETCH_COINS = "FETCH_COINS";
 export const SWITCH_THEME = "SWITCH_THEME";
+export const ADD_COIN = "ADD_COIN";
+export const REMOVE_COIN = "REMOVE_COIN";
