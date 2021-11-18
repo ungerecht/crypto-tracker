f8f81f547bc1c732ae87bb6573991882b007eb8c
ungerecht <kevinungerecht@gmail.com>
2021-11-17 16:20:13 -0800

    Add new types for watchlist actions

--- src/actions/types.js
+++ src/actions/types.js
@@ -2,5 +2,7 @@ export const SIGN_IN = "SIGN_IN";
 export const SIGN_OUT = "SIGN_OUT";
 export const FETCH_COINS = "FETCH_COINS";
 export const SWITCH_THEME = "SWITCH_THEME";
+export const GET_LIST = "GET_LIST";
 export const ADD_COIN = "ADD_COIN";
 export const REMOVE_COIN = "REMOVE_COIN";
+export const CLEAR_LIST = "CLEAR_LIST";
