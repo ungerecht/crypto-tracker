87a55228ac2c9b4625d99d8ea61faa522ce5a4ab
ungerecht <kevinungerecht@gmail.com>
2021-11-17 17:57:09 -0800

    Update baseURL to deployed site

--- src/apis/watchlists.js
+++ src/apis/watchlists.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:3001/watchlists",
+  baseURL: "https://crypto-tracker-json-server.herokuapp.com/watchlists",
 });
