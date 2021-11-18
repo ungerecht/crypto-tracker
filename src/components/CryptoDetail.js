4b8b920d96f1cd79ce4ef13d550026469ce1b2cb
ungerecht <kevinungerecht@gmail.com>
2021-11-17 16:22:08 -0800

    Move htmlDecode function

--- src/components/CryptoDetail.js
+++ src/components/CryptoDetail.js
@@ -14,6 +14,7 @@ import {
 } from "react-bootstrap";
 import "../styles/CryptoDetail.css";
 import { useSelector } from "react-redux";
+import { htmlDecode } from "../helpers";
 
 const CryptoDetail = (props) => {
   const { theme } = useSelector((state) => state.theme);
@@ -146,11 +147,6 @@ const renderStatsPlaceholders = () => {
     );
   }
   return placeholders;
-};
-
-const htmlDecode = (input) => {
-  var doc = new DOMParser().parseFromString(input, "text/html");
-  return doc.documentElement.textContent;
 };
 
 export default CryptoDetail;
