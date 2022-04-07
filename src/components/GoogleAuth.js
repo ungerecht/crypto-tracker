a3890d1ea23cecc2f1212035bfcd05064ef4b6b7
ungerecht <kevinungerecht@gmail.com>
2022-04-07 14:39:37 -0700

    Add in logs for debug

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -1,80 +1,82 @@
-import React from "react";
-import { useEffect } from "react";
-import { Button } from "react-bootstrap";
-import { useSelector, useDispatch } from "react-redux";
-import { MY_CLIENT_ID } from "../keys";
-import { signIn, signOut } from "../actions";
-import { googleIcon } from "../icons";
+import React from 'react'
+import { useEffect } from 'react'
+import { Button } from 'react-bootstrap'
+import { useSelector, useDispatch } from 'react-redux'
+import { MY_CLIENT_ID } from '../keys'
+import { signIn, signOut } from '../actions'
+import { googleIcon } from '../icons'
 
 const GoogleAuth = () => {
-  const isSignedIn = useSelector((state) => state.auth.isSignedIn);
-  const dispatch = useDispatch();
+  const isSignedIn = useSelector((state) => state.auth.isSignedIn)
+  const dispatch = useDispatch()
 
   useEffect(() => {
     const onAuthChange = (isSignedIn) => {
+      console.log('auth change')
       if (isSignedIn) {
         dispatch(
           signIn(window.gapi.auth2.getAuthInstance().currentUser.get().getId())
-        );
+        )
       } else {
-        dispatch(signOut());
+        dispatch(signOut())
       }
-    };
+    }
 
-    window.gapi.load("client:auth2", () => {
+    window.gapi.load('client:auth2', () => {
       window.gapi.client
         .init({
           clientId: MY_CLIENT_ID,
-          scope: "email",
+          scope: 'email',
         })
         .then(() => {
-          onAuthChange(window.gapi.auth2.getAuthInstance().isSignedIn.get());
-          window.gapi.auth2.getAuthInstance().isSignedIn.listen(onAuthChange);
-        });
-    });
-  }, [dispatch]);
+          onAuthChange(window.gapi.auth2.getAuthInstance().isSignedIn.get())
+          window.gapi.auth2.getAuthInstance().isSignedIn.listen(onAuthChange)
+        })
+    })
+  }, [dispatch])
 
   const renderAuthButton = () => {
     if (isSignedIn) {
       return (
         <Button
-          size="sm"
-          variant="outline-danger"
-          className="d-flex align-items-center bar-button"
+          size='sm'
+          variant='outline-danger'
+          className='d-flex align-items-center bar-button'
           onClick={() => {
-            dispatch(onSignOutClick);
+            dispatch(onSignOutClick)
           }}
         >
           {googleIcon}
-          <div className="ms-1">Log Out</div>
+          <div className='ms-1'>Log Out</div>
         </Button>
-      );
+      )
     } else {
       return (
         <Button
-          size="sm"
-          variant="outline-primary"
-          className="d-flex align-items-center bar-button"
+          size='sm'
+          variant='outline-primary'
+          className='d-flex align-items-center bar-button'
           onClick={() => {
-            dispatch(onSignInClick);
+            dispatch(onSignInClick)
           }}
         >
           {googleIcon}
-          <div className="ms-1">Log In</div>
+          <div className='ms-1'>Log In</div>
         </Button>
-      );
+      )
     }
-  };
+  }
 
-  return <div>{renderAuthButton()}</div>;
-};
+  return <div>{renderAuthButton()}</div>
+}
 
 const onSignInClick = () => {
-  window.gapi.auth2.getAuthInstance().signIn();
-};
+  console.log('sign in click')
+  window.gapi.auth2.getAuthInstance().signIn()
+}
 
 const onSignOutClick = () => {
-  window.gapi.auth2.getAuthInstance().signOut();
-};
+  window.gapi.auth2.getAuthInstance().signOut()
+}
 
-export default GoogleAuth;
+export default GoogleAuth
